eb8ece930577599b00013cc4ae21f8212f4936c2
Technotag <37962@ocvts.org>
2025-01-10 18:03:40 +0000

    Added buttons and some new operators

---
+++ script.js
@@ -0,0 +1,154 @@
+//These are the variables I used
+let userInput = '';
+let userOperator = '';
+let num1 = '';
+let num2 = '';
+
+// This function enables the user to input numbers by taken the number and adding it to calDisplay
+function calcAdd(num) {
+    userInput += num;
+    calcDisplay.innerHTML = userInput;
+}
+
+// The add function will add the first inputed number and the second one if the + button is clicked
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+// The subtract function will subtract the first inputed number and the second one if the - button is clicked
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+// The multiply function will multiply the first inputed number and the second one if the x button is clicked
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+// The divide function will divide the first inputed number and the second one if the ÷ button is clicked
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+// The lessThan function will determine if the second number is less than the first one when the < button is clicked
+function lessThan(num1, num2) {
+    return num1 <= num2;
+}
+
+// The greaterThan function will determine if the first number is greater than the second one when the > button is clicked
+function greaterThan(num1, num2) {
+    return num1 >= num2;
+}
+
+// The appendSquareRoot function will give the sqaure root of the first inputed number
+function appendSquareRoot() {
+    return Math.sqrt(userInput);
+  
+}
+
+// The modulous function will show the remander of the first and second inputed number when divided
+function modulous(num1, num2) {
+    return num1 % num2;
+}
+
+// The del function will delete all inputs and reset everything back to before. Simply making the inputs equal nothing and the display equal 0
+function del() {
+    num1 = '';
+    num2 = '';
+    userInput = '';
+    calcDisplay.innerHTML = 0;
+}
+
+//
+function decimal() {
+    if (!userInput.includes('.')) {
+        userInput += '.';
+        calcDisplay.innerHTML = userInput;
+    }
+}
+
+//
+function exponent() {
+
+}
+
+//
+function fraction() {
+
+}
+
+//
+function round() {
+    return Math.round(userInput);
+}
+
+//
+function parentheses() {
+
+}
+
+
+
+// The calculate function stores every possible number and variable, which allows them to be activated. It will also allow the calculation inputed by the user to be done.
+function calculate() {
+
+    num2 = parseInt(userInput);
+
+    switch(userOperator) { //This switch statement is what allows the buttons to properly work. Without this they won't work when pressed
+        case '+':
+            calcDisplay.innerHTML = add(num1, num2);
+            break;
+        case '-':
+            calcDisplay.innerHTML = subtract(num1, num2);
+            break;
+        case 'x':
+            calcDisplay.innerHTML = multiply(num1, num2);
+            break;
+        case '÷':
+            calcDisplay.innerHTML = divide(num1, num2);
+            break;
+        case '<':
+            calcDisplay.innerHTML = lessThan(num1,num2);
+            break;
+        case '>':
+            calcDisplay.innerHTML = greaterThan(num1,num2);
+            break;
+        case '√':
+            calcDisplay.innerHTML = appendSquareRoot(num1,num2);
+            break;
+        case '%':
+            calcDisplay.innerHTML = modulous(num1, num2);
+            break;
+        case '.':
+            calcDisplay.innerHTML = decimal(num1, num2);
+            break;
+         case '^':
+            calcDisplay.innerHTML = exponent(num1, num2);
+            break;
+        case '/':
+            calcDisplay.innerHTML = fraction(num1, num2);
+            break;
+         case '≈':
+            calcDisplay.innerHTML = round(num1, num2);
+            break;
+         case '(':
+            calcDisplay.innerHTML = parentheses(num1, num2);
+            break;
+        case ')':
+            calcDisplay.innerHTML = parentheses(num1, num2);
+            break;
+        case 'del':
+             calcDisplay.innerHTML = del();
+            break;
+        default:
+            calcDisplay.innerHTML = 'ERROR';
+            break;
+    }
+}
+
+// The operator function is what enables the operators to properly work. By having them allowed to be pressed by the user
+function operator (op) {
+    userOperator = op;
+    num1 = parseInt(userInput);
+    userInput= '';
+}
